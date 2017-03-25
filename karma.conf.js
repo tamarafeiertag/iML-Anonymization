d49da47ef4651bab50f446f82eaa6b66466e2a03
Tamara <tamara.feiertag@student.tugraz.at>
2017-03-25 11:29:34 +0100

    DEV: unit test fix

--- karma.conf.js
+++ karma.conf.js
@@ -8,8 +8,8 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'app/reusable_components/**/*.js',
-      'view*/**/*.js'
+      'reusable_components/**/*.js',
+      'components/**/*.js'
     ],
 
     autoWatch: true,
