6fb2015ceaa989912327a9026e0cfd283d955043
Tamara <tamara.feiertag@student.tugraz.at>
2017-03-28 07:30:16 +0200

    DEV: testdata working service

--- app/components/interactive-learning/interactive-learning-controller.js
+++ app/components/interactive-learning/interactive-learning-controller.js
@@ -2,7 +2,13 @@
 
 angular.module('iMLApp.interactive-learning.interactive-learning-controller', [])
 
-  .controller('ILCtrl', ['$scope', function ($scope) {
+  .controller('ILCtrl', function ($scope, ILService) {
+
+    console.log("ILCTrl");
+
+    ILService.getTestdata(function(testdata) {
+      console.log(testdata);
+    });
 
     $scope.up = function () {
       console.log("[ILCtrl] data record sent up");
@@ -10,4 +16,4 @@ angular.module('iMLApp.interactive-learning.interactive-learning-controller', []
     $scope.down = function () {
       console.log("[ILCtrl] data record sent down");
     }
-  }]);
+  });
--- app/components/interactive-learning/interactive-learning-service.js
+++ app/components/interactive-learning/interactive-learning-service.js
@@ -1,3 +1,16 @@
 'use strict';
 
 angular.module('iMLApp.interactive-learning.interactive-learning-service', [])
+
+.service('ILService', function ($resource) {
+  //use $resource later for retrieval from webservice
+
+
+  return {
+    getTestdata: function(callback) {
+      var testdata = null;
+      var TestdataResource = $resource('../assets/testdata/testdatarecords.json');
+      TestdataResource.get({}, callback);
+    }
+  }
+});
