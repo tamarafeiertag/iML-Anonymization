42be30eb1135728e65975a08af784a7af2e496f5
Tamara <tamara.feiertag@student.tugraz.at>
2017-03-31 16:55:29 +0200

    DEV: IL improved

--- app/components/interactive-learning/interactive-learning-controller.js
+++ app/components/interactive-learning/interactive-learning-controller.js
@@ -7,26 +7,60 @@ angular.module('iMLApp.interactive-learning.interactive-learning-controller', []
     $scope.columnNames = ["age", "education-num","hours-per-week", "workclass", "native-country", "sex", "race",
       "relationship","occupation","income", "marital-status"];
 
-    $scope.data = [];
+    $scope.allDataRecords = [];
+    $scope.dataTop = [];
+    $scope.dataBottom = [];
+    $scope.center;
+
+    var currentRecordIdx = 0;
 
     console.log("ILCTrl " + $scope.columnNames);
 
+    $scope.setRecords = function() {
+      $scope.dataTop = [];
+      $scope.dataBottom = [];
+      $scope.dataTop.push($scope.allDataRecords[currentRecordIdx]);
+      $scope.dataTop.push($scope.allDataRecords[currentRecordIdx+1]);
+      $scope.dataBottom.push($scope.allDataRecords[currentRecordIdx+2]);
+      $scope.dataBottom.push($scope.allDataRecords[currentRecordIdx+3]);
+      $scope.center = $scope.allDataRecords[currentRecordIdx+4];
+    };
 
+    ILService.getMaritalStatusK2().then(function(data) {
+      var countRecords = data.length;
+      for (var i = 0; i < countRecords; i++) {
+        data[i].id = i;
+      }
 
-    ILService.getMaritalStatusK2(function(data) {
-      console.log(data);
-      $scope.data = data;
+      $scope.allDataRecords = data;
+      $scope.setRecords();
+
+      console.log($scope.dataTop);
+      console.log($scope.dataBottom);
     });
 
-      ILService.getTestdata(function(data) {
-          console.log(data);
-          $scope.data = data;
-      });
 
     $scope.up = function () {
       console.log("[ILCtrl] data record sent up");
+      $("#panel-center").toggleClass("movedUp");
+      //currentRecordIdx += 6;
+      //$scope.setRecords();
     };
+
     $scope.down = function () {
       console.log("[ILCtrl] data record sent down");
-    }
+      $("#panel-center").toggleClass("movedDown");
+      //currentRecordIdx += 6;
+      //$scope.setRecords();
+    };
+
+    $scope.keyDown = function(value){
+      console.log(value.keyCode);
+      if(value.keyCode == 38) { //arrow up key pressed
+        $scope.up();
+      } else if(value.keyCode == 40) { //arrow down key pressed
+        $scope.down();
+      }
+    };
+
   });
--- app/components/interactive-learning/interactive-learning-service.js
+++ app/components/interactive-learning/interactive-learning-service.js
@@ -2,20 +2,19 @@
 
 angular.module('iMLApp.interactive-learning.interactive-learning-service', [])
 
-.service('ILService', function ($resource) {
+.factory('ILService', function ($resource) {
   //use $resource later for retrieval from webservice
 
+  var dataResource = $resource('assets/testdata/marital-status-k2.json');
+  var data = dataResource.query();
 
   return {
     getTestdata: function(callback) {
-      var testdata = null;
       var TestdataResource = $resource('assets/testdata/testdatarecords.json');
       TestdataResource.get(callback);
     },
-    getMaritalStatusK2: function(callback) {
-      var data = null;
-      var dataResource = $resource('assets/testdata/marital-status-k2.json');
-      dataResource.query(callback);
+    getMaritalStatusK2: function() {
+      return data.$promise;
     }
   }
 });
