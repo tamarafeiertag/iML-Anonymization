79989c567dffb7bde97a1e7f786525b5be7253ff
Julian Wolf <julian.wolf@student.tugraz.at>
2017-04-10 19:02:00 +0200

    It is not possible to change the function from outside its scope. The module needs to be changed

--- app/components/interactive-learning/interactive-learning-service.js
+++ app/components/interactive-learning/interactive-learning-service.js
@@ -1,20 +1,20 @@
-/* browserify interactive-learning-service.js -o interactive-learning-service-bundle.js */
-
 'use strict';
 
-var anonymization = require('anonymiationjs').$G;
+
+let anonymization = require('anonymiationjs').$G;
+
 
 angular.module('iMLApp.interactive-learning.interactive-learning-service', [])
 
   .factory('ILService', function ($resource) {
     //use $resource later for retrieval from webservice
 
-    var dataResource = $resource('assets/testdata/marital-status-k2.json');
-    var data = dataResource.query();
+    let dataResource = $resource('assets/testdata/marital-status-k2.json');
+    let data = dataResource.query();
 
     return {
       getTestdata: function (callback) {
-        var TestdataResource = $resource('assets/testdata/testdatarecords.json');
+        let TestdataResource = $resource('assets/testdata/testdatarecords.json');
         TestdataResource.get(callback);
       },
       getMaritalStatusK2: function () {
@@ -23,8 +23,6 @@ angular.module('iMLApp.interactive-learning.interactive-learning-service', [])
       getAnonymizationRecords: function () {
 
          let adults = './assets/testdata/test_input/adult_data.csv';
-
-
 
           let weights = {
               'age': 0.95,
@@ -35,22 +33,11 @@ angular.module('iMLApp.interactive-learning.interactive-learning-service', [])
               'marital-status': 0.1
           };
 
-
-          window.fs.readFileSync = function(filename) {
-            let xhr = new XMLHttpRequest();
-              return xhr.open("GET", filename, true);
-          };
-
-
           let san = new anonymization.Algorithms.Sangreea("adults", adults, undefined, weights);
-
-          console.log('sa:', san);
           san.instantiateGraph();
           san.anonymizeGraph(10);
 
-          // let anonymizedOutfileRace = "race_weights_" + (+new Date()).toString();
-          // san.outputAnonymizedCSV(anonymizedOutfileRace);
-          // expect(san._graph.nrNodes()).to.equal(300);
+
 
       }
     }
