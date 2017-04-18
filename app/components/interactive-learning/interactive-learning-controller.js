1a14747188c5b31acc358d2ce10485adc935e930
Tamara <tamara.feiertag@student.tugraz.at>
2017-04-18 21:25:06 +0200

    ILService -> calculate data records, clusters, random weight vectors,...

--- app/components/interactive-learning/interactive-learning-controller.js
+++ app/components/interactive-learning/interactive-learning-controller.js
@@ -2,61 +2,89 @@
 
 angular.module('iMLApp.interactive-learning.interactive-learning-controller', [])
 
-  .controller('ILCtrl', function ($scope, ILService) {
+  .controller('ILCtrl', function ($scope, $q, ILService) {
 
-    ILService.getAnonymizationRecords();
+
 
     $scope.showDiagram = false;
     $scope.showTooltipFirst = false;
     $scope.showTooltipSecond = false;
 
+    $scope.formatNumber = function(i) {
+      return Math.round(i * 10000)/10000;
+    };
+
     $scope.columnNames = ["age", "education-num","hours-per-week", "workclass", "native-country", "sex", "race",
       "relationship","occupation","income", "marital-status"];
-    $scope.weights = ["0.1", "0.1","0.1", "0.1","0.1", "0.1", "0.1", "0.1", "0.1","0.1","0.1"];
+    //$scope.weights = ILService.getWeightsArray();
 
-    $scope.allDataRecords = [];
+    $scope.allCases = [];
     $scope.dataTop = [];
     $scope.dataBottom = [];
-    $scope.center;
+    $scope.weightVecTop = [];
+    $scope.weightVecBottom = [];
+    $scope.center = {};
     $scope.movedUp = false;
     $scope.movedDown = false;
 
-    var currentRecordIdx = 0;
-
-    console.log("ILCTrl " + $scope.columnNames);
+    let currentRecordIdx = 0;
+    let currentCenterIdx = 0;
 
     $scope.setRecords = function() {
       $scope.dataTop = [];
       $scope.dataBottom = [];
-      $scope.dataTop.push($scope.allDataRecords[currentRecordIdx]);
-      $scope.dataTop.push($scope.allDataRecords[currentRecordIdx+1]);
-      $scope.dataBottom.push($scope.allDataRecords[currentRecordIdx+2]);
-      $scope.dataBottom.push($scope.allDataRecords[currentRecordIdx+3]);
-      $scope.center = $scope.allDataRecords[currentRecordIdx+4];
+      if ($scope.allCases.length > currentRecordIdx) {
+        //console.log("current case idx: ", currentRecordIdx);
+        $scope.center = $scope.allCases[currentRecordIdx][0]._features;
+        $scope.dataTop.push($scope.allCases[currentRecordIdx][1]);
+        $scope.dataTop.push($scope.allCases[currentRecordIdx][1]);
+        $scope.dataBottom.push($scope.allCases[currentRecordIdx][2]);
+        $scope.dataBottom.push($scope.allCases[currentRecordIdx][2]);
+        $scope.weightVecTop = ILService.getWeightsArray($scope.allCases[currentRecordIdx][1].weights);
+        $scope.weightVecBottom = ILService.getWeightsArray($scope.allCases[currentRecordIdx][2].weights);
+      }
     };
 
-    ILService.getMaritalStatusK2().then(function(data) {
-      var countRecords = data.length;
-      for (var i = 0; i < countRecords; i++) {
-        data[i].id = i;
-      }
+/*
+    var promise_anonymizedRecords = ILService.getAnonymizedRecords();
+    var promise_notAnonymizedRecords = ILService.getNotAnonymizedRecords();
 
-      $scope.allDataRecords = data;
+
+    $q.all([promise_anonymizedRecords, promise_notAnonymizedRecords]).then(function (values) {
+      let clusters = values[0];
+      let centers = values[1];
+
+      console.log("clusters", clusters);
+      console.log("centers", centers);
+      $scope.allDataRecords = clusters;
+      $scope.allCenters = centers;
       $scope.setRecords();
 
-      console.log($scope.dataTop);
-      console.log($scope.dataBottom);
+    }, function(reason) {              //error
+      console.log("ILCTrl " + "[error] retrieving of anonymized clusters and centers failed: ", reason);
+    });*/
+
+    var promise_case = ILService.getCases(5, 20, 2);
+
+    promise_case.then(function (data) {
+      console.log("received data: ", data);
+      $scope.allCases = data;
+      $scope.setRecords();
+    }, function(reason) {              //error
+      console.log("ILCTrl " + "[error] retrieving of anonymized clusters and centers failed: ", reason);
     });
+
 
     var centerRecordTag = $("#panel-center");
 
     $scope.up = function () {
       console.log("[ILCtrl] data record sent up");
       $scope.movedUp = true;
-      centerRecordTag.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+      centerRecordTag.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd',
         function(e) {
           // code to execute after transition ends
-          currentRecordIdx += 6;
+          currentRecordIdx += 1;
+          currentCenterIdx += 1;
           $scope.setRecords();
           $scope.movedUp = false;
           $scope.$digest();
@@ -66,10 +94,11 @@ angular.module('iMLApp.interactive-learning.interactive-learning-controller', []
     $scope.down = function () {
       console.log("[ILCtrl] data record sent down");
       $scope.movedDown = true;
-      centerRecordTag.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+      centerRecordTag.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd',
       function(e) {
         // code to execute after transition ends
-        currentRecordIdx += 6;
+        currentRecordIdx += 1;
+        currentCenterIdx += 1;
         $scope.setRecords();
         $scope.movedDown = false;
         $scope.$digest();
