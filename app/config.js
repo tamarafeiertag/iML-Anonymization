b4a9522dad0d57f17e44576a0606ee56af0aa810
Tamara <tamara.feiertag@student.tugraz.at>
2017-05-31 16:02:10 +0200

    resetted config to default values

--- app/config.js
+++ app/config.js
@@ -25,8 +25,8 @@ angular.module('iMLApp.config', [])
         "target_income"
       ],
       gen_base: 'assets/genHierarchies/',
-      nrOfCases: 1,  //number of triples per k factor
-      startKFactor: 5,
+      nrOfCases: 5,  //number of triples per k factor
+      startKFactor: 2,
       maxKFactor: 7,
       finalCSVStringKFactor: 7,
       finalCSVNrDraws: 500
@@ -36,15 +36,15 @@ angular.module('iMLApp.config', [])
   .constant(
     'anonymizationConfig', {
       'REMOTE_URL': 'http://berndmalle.com/anonymization/adults',
-      'REMOTE_TARGET': 'education', //TODO: how to handle that?
+      'REMOTE_TARGET': 'education',
       'INPUT_FILE': './assets/00_sample_data_UI_prototype/original_data_500_rows.csv',
       'TRIM': '\\s+',
       'TRIM_MOD': 'g',
       'SEPARATOR': ',',
       'SEP_MOD': '',
-      'TARGET_COLUMN': 'education-num', //TODO: how to handle that?
+      'TARGET_COLUMN': 'education-num',
       'AVERAGE_OUTPUT_RANGES': false,
-      'NR_DRAWS': 300,
+      'NR_DRAWS': 500,
       'RANDOM_DRAWS': false,  //must be false if center should not be picked from data points that might be anonymized
       'EDGE_MIN': 2,
       'EDGE_MAX': 10,
